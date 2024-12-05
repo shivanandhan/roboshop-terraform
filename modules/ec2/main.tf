ccd69f69dd31dd082b5bc4d3cd62daeaa0b70a39
Shiva <shivanandhan33@gmail.com>
2024-12-05 16:51:35 +0530

    terraform roboshop

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "instance" {
 # provisioner "local-exec" {
 #   command = <<EOL
 #   cd /home/ec2-user/roboshop-ansible
-#   ansible-playbook -i ${self.private_ip},-e ansible_user=ec2-user ansible_password=DevOps321 -e app_name=${var.component_name} -e env=${var.env} main.yml
+#   ansible-playbook -i ${self.private_ip},-e ansible_user=ec2-user ansible_password=DevOps321 -e app_name=${var.component_name} -e env=${var.env} roboshop.yml
 #    EOL
 #   }
 
