19ba70d8da077f377b34443c4e1f49ff43e17d7d
Shiva <shivanandhan33@gmail.com>
2024-12-10 14:22:28 +0530

    terraform roboshop

--- sivaji_roboshop-ansible/run.sh
+++ sivaji_roboshop-ansible/run.sh
@@ -1,7 +1,7 @@
 git pull;
 if[$# -ne 2]; then
-  echo input missing
-  echo usage: bash run.sh <component-name> <env>
+  echo "input missing"
+  echo "usage: bash run.sh <component-name> <env>"
   exit 1
 fi
 ansible-playbook -i $1-$2.kndevops72.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev  -e app_name=$1 roboshop.yml
