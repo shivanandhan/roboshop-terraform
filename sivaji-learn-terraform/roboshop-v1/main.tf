2cbb0ef30b70944b7c2c530eb40d9fc283595773
Shiva <shivanandhan33@gmail.com>
2024-12-12 11:48:53 +0530

    terraform roboshop

--- sivaji-learn-terraform/roboshop-v1/main.tf
+++ sivaji-learn-terraform/roboshop-v1/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0c6e17b6efba619a0"]
   tags={
     Name = "Frontend"
   }
