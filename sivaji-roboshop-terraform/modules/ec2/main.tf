6cb0b7538b2b8201220f814850e5c51ef72f9220
Shiva <shivanandhan33@gmail.com>
2024-12-21 12:51:09 +0530

    terraform roboshop

--- sivaji-roboshop-terraform/modules/ec2/main.tf
+++ sivaji-roboshop-terraform/modules/ec2/main.tf
@@ -10,8 +10,8 @@ resource "aws_security_group" "sg" {
   }
 
   ingress {
-    from_port        = var.app_port
-    to_port          = var.app_port
+    from_port        = 22
+    to_port          = 22
     protocol         = "TCP"
     cidr_blocks      = ["0.0.0.0/0"]
   }
